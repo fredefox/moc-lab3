4b05f90085019e41835305f03f418868020e19dd
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2016-11-21 17:09:13 +0100

    Implement substitution

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -4,5 +4,19 @@ module Interpreter
 
 import Chi
 
+subst :: Variable -> Exp -> Exp -> Exp
+subst v e t = case t of
+  Apply e0 e1  -> Apply (subst v e e0) (subst v e e1)
+  Lambda v0 e0 -> if v == v0 then t else (Lambda v0 (subst v e e0))
+  Case e0 bs   -> Case (subst v e e0) (map substB bs)
+    where
+      substB b@(Branch c vs e1) =
+        if any (== v) vs
+        then b
+        else Branch c vs (subst v e e1)
+  Rec v0 e0    -> if v == v0 then t else (Rec v0 (subst v e e0))
+  Var v0       -> if v == v0 then e else t
+  Const _ _    -> t
+
 interpret :: IO ()
 interpret = error "Unimplemented"

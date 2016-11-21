a80d7b49458c9b1d3e9e178c1070ce3e61e39080
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2016-11-21 16:32:31 +0100

    Add sample program

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Chi
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+-- `foo` implements equality tests on natural numbers.
+sample = parse . unlines $
+  [ "rec foo = λm. λn. case m of"
+  , "  { Zero() -> case n of"
+  , "    { Zero()  -> True()"
+  , "    ; Succ(n) -> False()"
+  , "    }"
+  , "  ; Succ(m) -> case n of"
+  , "    { Zero()  -> False()"
+  , "    ; Succ(n) -> foo m n"
+  , "    }"
+  , "  }"
+  ]
